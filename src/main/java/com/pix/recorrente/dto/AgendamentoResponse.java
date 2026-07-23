f4c123be2faaa52a50f89d3f7ab23efe892221c0
Guilherme Castro <guilherme.castro.fernandes@gmail.com>
2026-07-23 19:45:19 -0300

    chore: hide dataExecucao when null

--- src/main/java/com/pix/recorrente/dto/AgendamentoResponse.java
+++ src/main/java/com/pix/recorrente/dto/AgendamentoResponse.java
@@ -1,5 +1,6 @@
 package com.pix.recorrente.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pix.recorrente.domain.enums.EnumFrequencia;
 import com.pix.recorrente.domain.enums.EnumStatusAgendamento;
 import com.pix.recorrente.domain.enums.EnumStatusPagamento;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record AgendamentoResponse(
         UUID id,
         String clienteId,
